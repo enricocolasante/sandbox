ba006c3e45bacef064d90cb9861c637c98545cfc
Enrico Colasante <ecolasante@vsware.ie>
2017-12-02 10:30:56 +0000

    Web socket in Phoenix

--- elixir/discuss/web/controllers/topic_controller.ex
+++ elixir/discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,14 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
+
+  def show(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    render conn, "show.html", topic: topic
+  end
+
   def delete(conn, %{"id" => topic_id}) do
   	Repo.get!(Topic, topic_id) |> Repo.delete!
 
@@ -27,7 +35,7 @@ defmodule Discuss.TopicController do
 
   def edit(conn, %{"id" => topic_id}) do
   	topic = Repo.get(Topic, topic_id)
-	changeset = Topic.changeset(topic)
+	  changeset = Topic.changeset(topic)
   	
   	render conn, "edit.html", changeset: changeset, topic: topic
   end
@@ -46,7 +54,9 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-  	changeset = Topic.changeset(%Topic{}, topic)
+  	changeset = conn.assigns[:user]
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
     
     case Repo.insert(changeset) do
     	{:ok, post} ->
@@ -55,6 +65,19 @@ defmodule Discuss.TopicController do
     		|> redirect(to: topic_path(conn, :index))
     	{:error, changeset} -> 
     		render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that.")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
     end
   end
 end
--- elixir/discuss/web/static/js/socket.js
+++ elixir/discuss/web/static/js/socket.js
@@ -53,10 +53,48 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = (topicId) => {
+	let channel = socket.channel(`comments:${topicId}`, {})
+	channel.on(`comments:${topicId}:new`, renderComment);
+	channel.join()
+	  .receive("ok", resp => { 
+	  	console.log(resp);
+	  	renderComments(resp.comments);
+	  })
+	  .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+	document.querySelector('button').addEventListener('click', () => {
+		const content = document.querySelector('textarea').value;
+		channel.push('comment:add', {content: content});
+	});
+}
+
+function renderComments(comments) {
+	const renderComments = comments.map(comment =>{
+		return commentTemplate(comment);
+	});
+
+	document.querySelector('.collection').innerHTML = renderComments.join('');
+}
+
+function renderComment(event) {
+	const renderComment = commentTemplate(event.comment);
+	document.querySelector('.collection').innerHTML += renderComment;
+}
+
+function commentTemplate(comment) {
+	let email = 'Anonymous'
+	if(comment.user){
+		email = comment.user.email
+	}
+	return `
+			<li class="collection-item">
+			${comment.content}
+			<div class="secondary-content">
+			${email}
+			</div>
+			</li>
+		`;
+}
+
+window.createSocket = createSocket;
